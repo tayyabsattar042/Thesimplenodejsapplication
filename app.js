b03d1d7a1ac5a3bcd5883d100814396c344575f3
Muhammad Tayyab Sattar <tayyabsattar423@gmail.com>
2024-09-24 10:31:25 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const port = 3000; 
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
